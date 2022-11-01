f0a4f5c0ba0ba2b908832dc687855aa0dea6704f
francisco <franciscoobertoqgmail.com>
2022-10-31 21:33:09 -0300

    Filtrado de errores

--- Filter.py
+++ Filter.py
@@ -1,5 +1,7 @@
 from TestBot.indicators.Indicator import indicators
 from TestBot.Binance import Binance
+from TestBot.tools.price_system import PRICE
+from TestBot.Binance import temps
 
 
 def multireplace(string, lista, valor):
@@ -19,3 +21,19 @@ def check(string):
 
 def checkSymbol(symbol):
     assert symbol in Binance().getAllTickers(), 'UnknownSymbolException'
+
+
+def checkPriceSystem(system):
+    assert system in PRICE.keys(), 'UnknownPriceSystemException'
+
+
+def checkTemporality(temporality):
+    assert temporality in temps.keys(), 'UnknownTemporalityException'
+
+
+def checkPercentage(percentage):
+    if percentage is None or percentage.strip() is '':
+        return None
+    n = float(percentage)
+    assert 0 <= n <= 100, 'PercentageException'
+    return 1-float(percentage)/100 if n > 0 else None
